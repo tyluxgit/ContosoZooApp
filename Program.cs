b35b30aeb2add586c3c4b38dd8d975b6fe11d0fe
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-06 11:14:36 +0100

    Décommenter et ajouter la méthode RandomizeAnimals
    La méthode `RandomizeAnimals()` a été décommentée pour être exécutée. Une nouvelle implémentation de cette méthode a été ajoutée pour mélanger les éléments du tableau `pettingZoo`.

--- Program.cs
+++ Program.cs
@@ -4,8 +4,22 @@
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
-// RandomizeAnimals();
+RandomizeAnimals();
 // string[,] group = AssignGroup();
 Console.WriteLine("School A");
 
 // PrintGroup(group);
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+}
