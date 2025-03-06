3d60cde3f97e7873ef8b0e9f85baa0cd755c9021
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-06 10:47:10 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] pettingZoo =
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
