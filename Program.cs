bbbd95a17d1bbf51301006efb9dcbcf06c891e09
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-06 11:42:44 +0100

    Ajout de la méthode AssignGroup pour regrouper les animaux
    Implémentation d'une nouvelle méthode `AssignGroup` dans Program.cs pour répartir les animaux du zoo en groupes. Cette méthode crée un tableau à deux dimensions et remplit chaque groupe avec les animaux du tableau `pettingZoo`. Mise à jour du code pour appeler cette méthode et stocker le résultat dans la variable `group`, remplaçant une ligne de code commentée.

--- Program.cs
+++ Program.cs
@@ -4,11 +4,7 @@
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
-RandomizeAnimals();
-// string[,] group = AssignGroup();
-Console.WriteLine("School A");
 
-// PrintGroup(group);
 
 void RandomizeAnimals()
 {
@@ -23,3 +19,38 @@ void RandomizeAnimals()
         pettingZoo[r] = temp;
     }
 }
+string[,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length / groups];
+
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
+}
+
+void PrintGroup(string[,] group)
+{
+    for (int i = 0; i < group.GetLength(0); i++)
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++)
+        {
+            Console.Write($"{group[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+RandomizeAnimals();
+string[,] group = AssignGroup();
+Console.WriteLine("School A");
+
+// PrintGroup(group);
